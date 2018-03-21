8e407d1131af6e663405e33bd22658d1ffe8fbb5
Leonardo <leoroberto.alves@hotmail.com>
2018-03-21 10:36:13 -0300

    Add pasta public ao projeto

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gerar PDF com PHP</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="jumbotron">
+        <h1>Gerar PDF com mpdf em PHP</h1>
+        <a class="btn btn-dark" href="gerarPDF.php" target="_blanck">Gerar PDF</a></button>
+    </div>
+</body>
+</html>
