9ed425c33b48e18d22855eb05aa71becb31e3d9e
Leonardo <leoroberto.alves@hotmail.com>
2018-03-21 13:46:53 -0300

    Alteracoes

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+    <?php  $body; ?>
     <div class="jumbotron">
         <h1>Gerar PDF com mpdf em PHP</h1>
         <a class="btn btn-dark" href="gerarPDF.php" target="_blanck">Gerar PDF</a></button>
